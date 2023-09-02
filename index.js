9ba02615cad374a1b806a5b88e64bd05f014a4e8
Rubinder Singh <robinsandhu1857@gmail.com>
2023-09-02 16:06:26 +0530

    FCC Backend Project 5
    This project is to analyse uploaded file and send back file name, mimetype and file size.

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+
+
 require('dotenv').config()
 
 var app = express();
 
+
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage: storage});
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  console.log("In the API call");
+  const file = req.file;
+  
+  if (!file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  // File details
+  const filename = file.originalname;
+  const size = file.size;
+  const mimetype = file.mimetype;
+
+  res.send({
+    name: filename,
+    type: mimetype,
+    size: size
+  });
 });
 
 
